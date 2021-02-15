826963b9d14ee8b6166b7bdf78f0fb5b5c90b4a8
Wayne Izatt <icebattle@gmail.com>
2021-02-15 10:09:25 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Sum:", indexSumTrick(1000000000))
+
+	rand.Seed(time.Now().Unix())
+	num := rand.Int()
+	num = num >> 56
+	fmt.Println("Hello,", num)
+	os.Exit(num)
+}
+
+func indexSum(size int) int {
+	var s int
+	for i := 0; i < size; i++ {
+		s += i
+	}
+	return s
+}
+
+func indexSumTrick(size int) int {
+	a := size - 1
+	b := size - 2
+	mul := a * b // this is a commen t
+	return size + (mul >> 1) - 1
+}
